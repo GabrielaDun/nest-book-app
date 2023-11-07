bca79dd35ddf10378c72c123722d88c45650fd29
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-11-07 12:59:05 +0100

    Started registartion

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/RegisterDTO.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+  @Post('/register')
+  create(@Body() userData: RegisterDTO) {
+    return this.authService.register(userData);
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,14 +3,12 @@ import {
   Get,
   NotFoundException,
   ParseUUIDPipe,
-  Post,
   Param,
   Body,
   Delete,
   Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDTO } from './dtos/create-users.dto';
 import { UpdateUserDTO } from './dtos/update-users.dto';
 
 @Controller('users')
@@ -24,11 +22,6 @@ export class UsersController {
   async getById(@Param('id', new ParseUUIDPipe()) id: string) {
     const user = await this.usersService.getById(id);
     if (!user) throw new NotFoundException('User not found');
-  }
-  @Post('/')
-  create(@Body() userData: CreateUserDTO) {
-    const { email, hashedPassword } = userData;
-    return this.usersService.create(email, hashedPassword);
   }
   @Delete('/:id')
   async delete(@Param('id', new ParseUUIDPipe()) id: string) {
